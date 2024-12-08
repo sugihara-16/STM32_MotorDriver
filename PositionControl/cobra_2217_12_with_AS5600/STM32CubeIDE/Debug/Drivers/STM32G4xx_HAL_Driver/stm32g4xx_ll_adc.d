4c1820a5970a2e7a765d6644df69c0aed6a0b95b
Junichiro Sugihara <j-sugihara@jsk.imi.i.u-tokyo.ac.jp>
2024-12-08 22:11:56 +0900

    checkout all debug files from git management

--- PositionControl/cobra_2217_12_with_AS5600/STM32CubeIDE/Debug/Drivers/STM32G4xx_HAL_Driver/stm32g4xx_ll_adc.d
+++
@@ -1,2 +0,0 @@
-Drivers/STM32G4xx_HAL_Driver/stm32g4xx_ll_adc.o: \
- C:/Users/junichiro/STM32_MotorDriver/PositionControl/cobra_2217_12_with_AS5600/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_ll_adc.c
--- SpeedControl/cobra_2217_12/STM32CubeIDE/Debug/Drivers/STM32G4xx_HAL_Driver/stm32g4xx_ll_adc.d
+++
@@ -1,2 +0,0 @@
-Drivers/STM32G4xx_HAL_Driver/stm32g4xx_ll_adc.o: \
- C:/Users/junichiro/STM32_MotorDriver/SpeedControl/cobra_2217_12/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_ll_adc.c
