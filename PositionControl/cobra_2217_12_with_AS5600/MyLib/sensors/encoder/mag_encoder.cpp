cad2cf65e357f3dfdee7a9273eceb0b7de828296
Junichiro Sugihara <j-sugihara@jsk.imi.i.u-tokyo.ac.jp>
2024-12-11 14:32:30 +0900

    [Position Controller] Modified indent.

--- PositionControl/cobra_2217_12_with_AS5600/MyLib/sensors/encoder/mag_encoder.cpp
+++ PositionControl/cobra_2217_12_with_AS5600/MyLib/sensors/encoder/mag_encoder.cpp
@@ -9,14 +9,14 @@
 #define UPDATE_INTERVAL 10
 
 MagEncoder::MagEncoder()
-    : i2cHandle_(nullptr), i2cAddr_((AS5600_SLAVE_ADDRESS & 0x7F) << 1),
-      angle_(0), rawAngle_(0), magneticMagnitude_(0), agc_(0), magnetDetected_(false) {}
+  : i2cHandle_(nullptr), i2cAddr_((AS5600_SLAVE_ADDRESS & 0x7F) << 1),
+    angle_(0), rawAngle_(0), magneticMagnitude_(0), agc_(0), magnetDetected_(false) {}
 
 HAL_StatusTypeDef MagEncoder::init(I2C_HandleTypeDef *i2cHandle) {
-    i2cHandle_ = i2cHandle;
-    last_time_ = HAL_GetTick() + 6000; // after 6s
-    device_id_ = 128;
-    return HAL_OK;
+  i2cHandle_ = i2cHandle;
+  last_time_ = HAL_GetTick() + 6000; // after 6s
+  device_id_ = 128;
+  return HAL_OK;
 }
 
 HAL_StatusTypeDef MagEncoder::update() {
@@ -28,61 +28,61 @@ HAL_StatusTypeDef MagEncoder::update() {
       device_id_ = i;
     }
   }
-    uint8_t data[2] = {0};
+  uint8_t data[2] = {0};
  
-    // RAW ANGLE
-    if (readRegister(AS5600_RAWANGLE_REG_ADDR, data, 2) == HAL_OK) {
-        rawAngle_ = (data[0] << 8) | data[1];
-    }
+  // RAW ANGLE
+  if (readRegister(AS5600_RAWANGLE_REG_ADDR, data, 2) == HAL_OK) {
+    rawAngle_ = (data[0] << 8) | data[1];
+  }
 
-    // ANGLE
-    if (readRegister(AS5600_ANGLE_REG_ADDR, data, 2) == HAL_OK) {
-      uint16_t inverse_angle = (data[0] << 8) | data[1];
-      angle_ = 4096 - inverse_angle;
-   }
+  // ANGLE
+  if (readRegister(AS5600_ANGLE_REG_ADDR, data, 2) == HAL_OK) {
+    uint16_t inverse_angle = (data[0] << 8) | data[1];
+    angle_ = 4096 - inverse_angle;
+  }
 
-    // MAGNITUDE
-    if (readRegister(AS5600_MAG_REG_ADDR, data, 2) == HAL_OK) {
-        magneticMagnitude_ = (data[0] << 8) | data[1];
-    }
+  // MAGNITUDE
+  if (readRegister(AS5600_MAG_REG_ADDR, data, 2) == HAL_OK) {
+    magneticMagnitude_ = (data[0] << 8) | data[1];
+  }
 
-    // AGC
-    if (readRegister(AS5600_AGC_REG_ADDR, data, 1) == HAL_OK) {
-        agc_ = data[0];
-    }
+  // AGC
+  if (readRegister(AS5600_AGC_REG_ADDR, data, 1) == HAL_OK) {
+    agc_ = data[0];
+  }
 
-    // MAGNET STATUS
-    if (readRegister(AS5600_STATUS_REG_ADDR, data, 1) == HAL_OK) {
-        magnetDetected_ = (data[0] & (1 << 5)) != 0;
-    }
+  // MAGNET STATUS
+  if (readRegister(AS5600_STATUS_REG_ADDR, data, 1) == HAL_OK) {
+    magnetDetected_ = (data[0] & (1 << 5)) != 0;
+  }
 
-    return HAL_OK;
+  return HAL_OK;
 }
 
 uint16_t MagEncoder::getAngle() const {
-    return angle_;
+  return angle_;
 }
 
 uint16_t MagEncoder::getRawAngle() const {
-    return rawAngle_;
+  return rawAngle_;
 }
 
 uint16_t MagEncoder::getMagneticMagnitude() const {
-    return magneticMagnitude_;
+  return magneticMagnitude_;
 }
 
 uint8_t MagEncoder::getAGC() const {
-    return agc_;
+  return agc_;
 }
 
 bool MagEncoder::isMagnetDetected() const {
-    return magnetDetected_;
+  return magnetDetected_;
 }
 
 HAL_StatusTypeDef MagEncoder::readRegister(uint8_t regAddr, uint8_t *data, uint16_t len) {
-    return HAL_I2C_Mem_Read(i2cHandle_, i2cAddr_, regAddr, I2C_MEMADD_SIZE_8BIT, data, len, HAL_MAX_DELAY);
+  return HAL_I2C_Mem_Read(i2cHandle_, i2cAddr_, regAddr, I2C_MEMADD_SIZE_8BIT, data, len, HAL_MAX_DELAY);
 }
 
 HAL_StatusTypeDef MagEncoder::writeRegister(uint8_t regAddr, uint8_t *data, uint16_t len) {
-    return HAL_I2C_Mem_Write(i2cHandle_, i2cAddr_, regAddr, I2C_MEMADD_SIZE_8BIT, data, len, HAL_MAX_DELAY);
+  return HAL_I2C_Mem_Write(i2cHandle_, i2cAddr_, regAddr, I2C_MEMADD_SIZE_8BIT, data, len, HAL_MAX_DELAY);
 }
--- PositionControl/cobra_2217_12_with_AS5600/MyLib/sensors/encoder/mag_encoder.h
+++ PositionControl/cobra_2217_12_with_AS5600/MyLib/sensors/encoder/mag_encoder.h
@@ -5,35 +5,34 @@
 #include "stm32g4xx_hal.h"
 
 class MagEncoder {
-public:
-    MagEncoder();
+ public:
+  MagEncoder();
 
-    HAL_StatusTypeDef init(I2C_HandleTypeDef *i2cHandle);
+  HAL_StatusTypeDef init(I2C_HandleTypeDef *i2cHandle);
 
-    HAL_StatusTypeDef update();
+  HAL_StatusTypeDef update();
 
-    uint16_t getAngle() const;
-    uint16_t getRawAngle() const;
-    uint16_t getMagneticMagnitude() const;
-    uint8_t getAGC() const;
-    bool isMagnetDetected() const;
+  uint16_t getAngle() const;
+  uint16_t getRawAngle() const;
+  uint16_t getMagneticMagnitude() const;
+  uint8_t getAGC() const;
+  bool isMagnetDetected() const;
 
-private:
-    I2C_HandleTypeDef *i2cHandle_;
-    uint8_t i2cAddr_;
-    uint32_t i2c_error_code_;
+ private:
+  I2C_HandleTypeDef *i2cHandle_;
+  uint8_t i2cAddr_;
+  uint32_t i2c_error_code_;
 
-    uint32_t last_time_;
-    uint16_t device_id_;
+  uint32_t last_time_;
+  uint16_t device_id_;
+  uint16_t angle_;
+  uint16_t rawAngle_;
+  uint16_t magneticMagnitude_;
+  uint8_t agc_;
+  bool magnetDetected_;
 
-    uint16_t angle_;
-    uint16_t rawAngle_;
-    uint16_t magneticMagnitude_;
-    uint8_t agc_;
-    bool magnetDetected_;
-
-    HAL_StatusTypeDef readRegister(uint8_t regAddr, uint8_t *data, uint16_t len);
-    HAL_StatusTypeDef writeRegister(uint8_t regAddr, uint8_t *data, uint16_t len);
+  HAL_StatusTypeDef readRegister(uint8_t regAddr, uint8_t *data, uint16_t len);
+  HAL_StatusTypeDef writeRegister(uint8_t regAddr, uint8_t *data, uint16_t len);
 };
 
 #endif // MAG_ENCODER_H_
